a564e7a0165721ad20b236b94075fac8bf6c9360
HikaruShigihara <zeal19300035@c-league.jp>
2020-11-13 16:27:17 +0900

    プレイヤーの追加

--- BaseCross/projectData/GameSources/Player.cpp
+++ BaseCross/projectData/GameSources/Player.cpp
@@ -9,8 +9,45 @@
 namespace basecross{
 
 	void Player::OnCreate() {
+		//初期位置などの設定
+		auto ptr = AddComponent<Transform>();
+		ptr->SetScale(0.25f, 0.25f, 0.25f);	//直径25センチの球体
+		ptr->SetRotation(0.0f, 0.0f, 0.0f);
+		ptr->SetPosition(Vec3(0, 1.50f, 0));
 
+		//CollisionSphere衝突判定を付ける
+		auto ptrColl = AddComponent<CollisionSphere>();
 
+		//各パフォーマンスを得る
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+		//重力をつける
+		auto ptrGra = AddComponent<Gravity>();
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+		//文字列をつける
+		//auto ptrString = AddComponent<StringSprite>();
+		//ptrString->SetText(L"");
+		//ptrString->SetTextRect(Rect2D<float>(16.0f, 16.0f, 640.0f, 480.0f));
+
+		//影をつける（シャドウマップを描画する）
+		auto shadowPtr = AddComponent<Shadowmap>();
+		//影の形（メッシュ）を設定
+		shadowPtr->SetMeshResource(L"DEFAULT_SPHERE");
+
+		//描画コンポーネントの設定
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		//描画するメッシュを設定
+		ptrDraw->SetMeshResource(L"DEFAULT_SPHERE");
+		ptrDraw->SetFogEnabled(true);
+		//描画するテクスチャを設定
+		ptrDraw->SetTextureResource(L"Blue.png");
+		SetAlphaActive(true);
 	}
 
 }
