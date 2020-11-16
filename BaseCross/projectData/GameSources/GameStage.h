01ca9b7b7da10438a74cf73e6cc66346baaff2d4
human20002 <zeal19300005@c-league.jp>
2020-11-16 15:39:15 +0900

    xmlファイルの読み込み

--- BaseCross/projectData/GameSources/GameStage.h
+++ BaseCross/projectData/GameSources/GameStage.h
@@ -12,8 +12,16 @@ namespace basecross {
 	//	ゲームステージクラス
 	//--------------------------------------------------------------------------------------
 	class GameStage : public Stage {
+		//XMLリーダー
+		unique_ptr<XmlDocReader> m_XmlDocReader;
+
 		//ビューの作成
 		void CreateViewLight();
+		//ボックスの作成
+		void CreateFixedBox();
+		//XMLのオブジェクトの作成
+		void CreateXmlObjects();
+
 	public:
 		//構築と破棄
 		GameStage() :Stage() {}
@@ -24,6 +32,25 @@ namespace basecross {
 
 	};
 
+	//--------------------------------------------------------------------------------------
+//	class MoveBox : public GameObject;
+//--------------------------------------------------------------------------------------
+	class MoveBox : public GameObject {
+		Vec3 m_Scale;
+		Vec3 m_Rotation;
+		Vec3 m_Position;
+	public:
+		//構築と破棄
+		MoveBox(const shared_ptr<Stage>& StagePtr,
+			const Vec3& Scale,
+			const Vec3& Rotation,
+			const Vec3& Position
+		);
+		virtual ~MoveBox();
+		//初期化
+		virtual void OnCreate() override;
+		//操作
+	};
 
 }
 //end basecross
