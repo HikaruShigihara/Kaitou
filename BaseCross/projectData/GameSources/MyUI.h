3ab4344343ec85767258509fde5f1e97a44d4039
HikaruShigihara <zeal19300035@c-league.jp>
2020-11-30 10:33:02 +0900

    bmf読込、雲追加

--- BaseCross/projectData/GameSources/MyUI.h
+++ BaseCross/projectData/GameSources/MyUI.h
@@ -142,4 +142,80 @@ namespace basecross {
 		virtual void OnCreate() override;
 	};
 
+	class Number_UI : public GameObject {
+		int m_place;	//à 
+		int m_Score;	//êîéö
+		std::vector<VertexPositionColorTexture> vertices;
+		std::vector<uint16_t> indices;
+
+		Vec2 m_vertex;
+		float m_size;
+		Vec3 m_pos;
+		Vec3 m_scale;
+		Col4 m_color;
+		int m_layer;
+		wstring m_textures;
+
+
+	public:
+		Number_UI(const shared_ptr<Stage>& StagePtr,
+			const Vec2& vertex,
+			const Vec3& pos,
+			const Vec3& scale,
+			const int& layer,
+			const Col4& color,
+			const wstring& textures,
+			int place,
+			int score
+
+		)
+			: GameObject(StagePtr),
+			m_vertex(vertex),
+			m_pos(pos),
+			m_scale(scale),
+			m_layer(layer),
+			m_color(color),
+			m_textures(textures),
+			m_place(static_cast<int>(place)),
+			m_Score(score)
+
+		{
+		}
+		~Number_UI() {}
+
+
+		void OnCreate() override;
+		void OnUpdate2() override;
+
+		int GetCount() {
+			return m_Score;
+		}
+	};
+
+	class Cloud : public UI_Base {
+	public:
+
+		Cloud(const shared_ptr<Stage>& StagePtr,
+			const Vec2& vertex,
+			const Vec3& pos,
+			const Vec3& scale,
+			const int& layer,
+			const Col4& color,
+			const wstring& textures
+		) :
+			UI_Base(
+				StagePtr,
+				vertex,
+				pos,
+				scale,
+				layer,
+				color,
+				textures
+			)
+		{}
+		~Cloud() {}
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+	};
 }
--- BaseCross/projectData/GameSources/Object.cpp
+++ BaseCross/projectData/GameSources/Object.cpp
@@ -9,36 +9,179 @@ namespace basecross {
 		ptrTransform->SetRotation(m_Rotation);
 		ptrTransform->SetPosition(m_Position);
 		//OBB衝突j判定を付ける
-		auto ptrColl = AddComponent<CollisionObb>();
-		ptrColl->SetFixed(true);
+		//auto ptrColl = AddComponent<CollisionObb>();
+		//ptrColl->SetFixed(true);
+
+
 		//タグをつける
 		AddTag(L"FixedBox");
+		//影をつける（シャドウマップを描画する）
+		//auto shadowPtr = AddComponent<Shadowmap>();
+		////影の形（メッシュ）を設定
+		//shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
+		//auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		//ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+
+
+		Mat4x4 spanMat; // モデルとトランスフォームの間の差分行列
+		spanMat.affineTransformation(
+			Vec3(1.0f, 1.0f, 1.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f)
+		);
+
+		//影をつける（シャドウマップを描画する）
+		auto ptrShadow = AddComponent<Shadowmap>();
+		//影の形（メッシュ）を設定
+		ptrShadow->SetMeshResource(L"Grass.bmf");
+		ptrShadow->SetMeshToTransformMatrix(spanMat);
+
+		auto ptrDraw = AddComponent<BcPNTStaticModelDraw>();
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetMeshResource(L"Grass.bmf");
+		ptrDraw->SetMeshToTransformMatrix(spanMat);
+
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetAfterCollision(AfterCollision::Auto);	
+		ptrColl->SetMakedSize(Vec3(2.0f, 2.0f, 2.0f));
+		ptrColl->SetFixed(true);
+
+		auto ptrParent = m_Parent.lock();
+		if (ptrParent) {
+			//auto posTarget = ptrParent->GetComponent<Transform>()->GetPosition();
+			//posTarget += m_VecToParent;
+			//ptrTransform->SetPosition(posTarget);
+			auto posTarget = ptrParent->GetComponent<Transform>()->GetRotation();
+			posTarget += m_VecToParent;
+			ptrTransform->SetRotation(posTarget);
+
+		}
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetOwnShadowActive(true);
+
+		//PsBoxParam param(ptrTransform->GetWorldMatrix(), 0.0f, true, PsMotionType::MotionTypeActive);
+		//auto PsPtr = AddComponent<RigidbodyBox>(param);
+		//PsPtr->SetDrawActive(false);
+
+	}
+	void Goal::OnCreate() {
+		auto ptrTransform = GetComponent<Transform>();
+		ptrTransform->SetScale(m_Scale);
+		ptrTransform->SetRotation(m_Rotation);
+		ptrTransform->SetPosition(m_Position);
+		//OBB衝突j判定を付ける
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+
+
+		//タグをつける
+		AddTag(L"Goal");
 		//影をつける（シャドウマップを描画する）
 		auto shadowPtr = AddComponent<Shadowmap>();
 		//影の形（メッシュ）を設定
 		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
 		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
 		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		ptrDraw->SetTextureResource(L"red.png");
 
 		auto ptrParent = m_Parent.lock();
 		if (ptrParent) {
-			auto posTarget = ptrParent->GetComponent<Transform>()->GetPosition();
+			//auto posTarget = ptrParent->GetComponent<Transform>()->GetPosition();
+			//posTarget += m_VecToParent;
+			//ptrTransform->SetPosition(posTarget);
+			auto posTarget = ptrParent->GetComponent<Transform>()->GetRotation();
 			posTarget += m_VecToParent;
-			ptrTransform->SetPosition(posTarget);
-		}
+			ptrTransform->SetRotation(posTarget);
 
+		}
 		ptrDraw->SetFogEnabled(true);
 		ptrDraw->SetOwnShadowActive(true);
 
-		PsBoxParam param(ptrTransform->GetWorldMatrix(), 0.0f, true, PsMotionType::MotionTypeFixed);
-		auto PsPtr = AddComponent<RigidbodyBox>(param);
-		PsPtr->SetDrawActive(true);
+		//PsBoxParam param(ptrTransform->GetWorldMatrix(), 0.0f, true, PsMotionType::MotionTypeActive);
+		//auto PsPtr = AddComponent<RigidbodyBox>(param);
+		//PsPtr->SetDrawActive(false);
 
 	}
 
 	void FixedBox::SeekParent() {
-		
+		auto ptrTrans = GetComponent<Transform>();
+		auto pos = ptrTrans->GetRotation();
+		auto ptrParent = m_Parent.lock();
+		if (ptrParent) {
+			auto matParent = ptrParent->GetComponent<Transform>()->GetWorldMatrix();
+			matParent.scaleIdentity();
+			Mat4x4 mat;
+			mat.affineTransformation(
+				Vec3(1.0),
+				Vec3(0.0),
+				Vec3(0.0),
+				m_VecToParent
+			);
+			mat *= matParent;
+
+			auto posTarget = mat.transInMatrix();
+			auto v = Lerp::CalculateLerp(pos, posTarget, 0.0f, 1.0f, 0.2f, Lerp::rate::Linear);
+			ptrTrans->SetRotation(v);
+			ptrTrans->SetQuaternion(mat.quatInMatrix());
+		}
 	}
 
+	void FixedBox::OnUpdate() {
+		SeekParent();
+		//auto KeyState = App::GetApp()->GetInputDevice().GetKeyState();
+		//auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+		//auto PtrAction = AddComponent<Action>();
+		//PtrAction->AddRotateTo(1.0f, Vec3(0, 0, 90));
+		//PtrAction->SetLooped(false);
 
+		//if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A) {
+
+		//	PtrAction->Run();
+
+		//}
+		//if (KeyState.m_bPushKeyTbl['W']) {
+		//	PtrAction->Run();
+
+		//}
+	}
+
+	void ParentBox::OnCreate() {
+		auto ptrTransform = GetComponent<Transform>();
+		ptrTransform->SetScale(m_Scale);
+		ptrTransform->SetRotation(m_Rotation);
+		ptrTransform->SetPosition(m_Position);
+		//OBB衝突j判定を付ける
+		//auto ptrColl = AddComponent<CollisionObb>();
+		//ptrColl->SetFixed(true);
+		//タグをつける
+		AddTag(L"ParentBox");
+		//影をつける（シャドウマップを描画する）
+		auto shadowPtr = AddComponent<Shadowmap>();
+		//影の形（メッシュ）を設定
+		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
+		auto ptrDraw = AddComponent<BcPCTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		ptrDraw->SetAlpha(true);
+
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetOwnShadowActive(true);
+	}
+	void ParentBox::OnUpdate() {
+		auto KeyState = App::GetApp()->GetInputDevice().GetKeyState();
+		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+		auto PtrAction = AddComponent<Action>();
+		PtrAction->AddRotateBy(1.0f, Vec3(0, 0, 90));
+		PtrAction->SetLooped(false);
+
+		if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A) {
+
+			PtrAction->Run();
+
+		}
+		if (KeyState.m_bPushKeyTbl['W']) {
+
+		}
+
+	}
 }
