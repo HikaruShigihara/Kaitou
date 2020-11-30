18fdc9cf34f7610798f06ac07121cf06abc03d7d
human20002 <zeal19300005@c-league.jp>
2020-11-30 12:15:43 +0900

    リザルトのテスト

--- BaseCross/projectData/GameSources/ResultStage.cpp
+++ BaseCross/projectData/GameSources/ResultStage.cpp
@@ -23,13 +23,38 @@ namespace basecross {
 	}
 
 	void ResultStage::CreateUI() {
-		AddGameObject<StageSelect_UI>(
+		AddGameObject<Result_UI>(
 			Vec2(1920.0f, 1080.0f),
 			Vec3(0.0f, 0.0f, 0.0f),
 			Vec3(1.0f),
+			-1,
+			Col4(1.0f),
+			m_Result_image
+			);
+
+		//AddGameObject<Result_UI>(
+		//	Vec2(190.0f, 180.0f),
+		//	Vec3(19.0f, 107.0f, 0.0f),
+		//	Vec3(x),
+		//	0,
+		//	Col4(1.0f),
+		//	m_Result_StarOnly
+		//	);
+		AddGameObject<Result_UI>(
+			Vec2(190.0f, 180.0f),
+			Vec3(297.0f, 107.0f, 0.0f),
+			Vec3(1.0f),
 			0,
 			Col4(1.0f),
-			m_Result_Star3
+			m_Result_StarOnly
+			);
+		AddGameObject<Result_UI>(
+			Vec2(190.0f, 180.0f),
+			Vec3(-259.0f, 107.0f, 0.0f),
+			Vec3(1.0f),
+			0,
+			Col4(1.0f),
+			m_Result_StarOnly
 			);
 
 	}
@@ -44,6 +69,24 @@ namespace basecross {
 		}
 	}
 
+	void ResultStage::OnUpdate() {
+
+		m_Star01=
+			AddGameObject<Result_UI>(
+			Vec2(190.0f, 180.0f),
+			Vec3(19.0f, 107.0f, 0.0f),
+			Vec3(x),
+			0,
+			Col4(1.0f),
+			m_Result_StarOnly
+			);
+
+		if (x > 0) {
+			x -= 0.01f;
+
+		}
+		m_Star01->
+	}
 
 }
 //end basecross
--- BaseCross/projectData/GameSources/ResultStage.h
+++ BaseCross/projectData/GameSources/ResultStage.h
@@ -15,19 +15,27 @@ namespace basecross {
 		wstring m_Result_Star1;
 		wstring m_Result_Star2;
 		wstring m_Result_Star3;
+		wstring m_Result_StarOnly;
 
 	public:
+		float x = 1.0f;
+		std::shared_ptr<basecross::Result_UI> m_Star01;
+		std::shared_ptr<basecross::Result_UI> m_Star02;
+		std::shared_ptr<basecross::Result_UI> m_Star03;
+
 		ResultStage() : Stage(),
 			m_Result_image(L"Result.png"),
 			m_Result_Star1(L"Star_1.png"),
 			m_Result_Star2(L"Star_2.png"),
-			m_Result_Star3(L"Star_3.png")
+			m_Result_Star3(L"Star_3.png"),
+			m_Result_StarOnly(L"StarOnly.png")
+			
 		{}
 
 		virtual ~ResultStage() {}
 
 		virtual void OnCreate() override;
-		//virtual void OnUpdate() override;
+		virtual void OnUpdate() override;
 	};
 
 }
