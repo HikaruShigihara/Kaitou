fceece7af4a09bcc7413de7e4f2c0c958420ed55
HikaruShigihara <zeal19300035@c-league.jp>
2020-12-09 16:06:14 +0900

    テキスト、マスク追加

--- BaseCross/projectData/GameSources/GameStage.h
+++ BaseCross/projectData/GameSources/GameStage.h
@@ -24,9 +24,13 @@ namespace basecross {
 		shared_ptr<GameObject> m_Target;
 		shared_ptr<GameObject> m_Goal;
 		shared_ptr<GameObject> m_player;
+		shared_ptr<GameObject> m_mask;
+		shared_ptr<GameObject> m_text;
 		wstring m_back;
 		wstring m_number_UI;
+		wstring m_mask_UI;
 		wstring m_goal;
+		wstring m_text_UI;
 
 		wstring m_cloud,m_cloud1, m_cloud2, m_cloud3, m_cloud4;
 		wstring m_clear;
@@ -39,7 +43,7 @@ namespace basecross {
 		void CreateUI();
 		void CreateBack();
 		void CreateCloud();
-
+		void CreateMask();
 		//XMLのオブジェクトの作成
 		void CreateXmlObjects();
 
@@ -49,12 +53,14 @@ namespace basecross {
 			m_back(L"Blue_Back.png"),
 			m_number_UI(L"number.png"),
 			m_goal(L"red.png"),
+			m_mask_UI(L"Base.png"),
 			m_cloud1(L"Cloud Parts 1.png"),
 			m_cloud2(L"Cloud Parts 2.png"),
 			m_cloud3(L"Cloud Parts 3.png"),
 			m_cloud4(L"Cloud Parts 4.png"),
 			m_cloud(L"Cloud.png"),
 			m_clear(L"Clear.png"),
+			m_text_UI(L"スタートボタンで開始！"),
 			m_number(000),
 			m_position(0.65f),
 			m_bgm(NULL)
